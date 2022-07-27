6d3a0866cdac1b1f3c5f092712419228a3055a76
Mat Booth <mat.booth@gmail.com>
2022-07-27 15:19:39 +0100

    Add tidal 3D helpers module
    This module contains fast, native implementations of maths functions
    commonly used in 3D rendering. These are about an order of magnitude
    faster than the same functions written in pure python.

---
+++ firmware_build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e -o pipefail
+
+TIDAL_DIR=$(cd $(dirname $0) && pwd)
+
+source ../esp-idf/export.sh
+
+export IOT_SOLUTION_PATH=$TIDAL_DIR/esp-iot-solution
+export V=1
+
+pushd $TIDAL_DIR
+./scripts/firstTime.sh
+
+pushd micropython
+make -C mpy-cross
+
+pushd ports/esp32
+(cd boards && ln -sfn $TIDAL_DIR/tildamk6 ./tildamk6)
+
+make submodules BOARD=tildamk6 USER_C_MODULES=$TIDAL_DIR/drivers/micropython.cmake
+make BOARD=tildamk6 USER_C_MODULES=$TIDAL_DIR/drivers/micropython.cmake clean all
+popd
+popd
+popd
